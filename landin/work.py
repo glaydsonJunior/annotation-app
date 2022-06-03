119814dc3963ac2854f81eb3c53410a86167a062
FreakW <102086284+FreakW@users.noreply.github.com>
2022-06-03 01:13:50 -0300

    Update work.py

--- landin/work.py
+++ landin/work.py
@@ -2,30 +2,33 @@ from flask import Flask, render_template, url_for, request, session, redirect
 from flask_session import Session
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+storage = []
 
 
 @app.route('/', methods=['POST','GET'])
 def notes():
-    if session.get('notas') is None:
-        session["notas"] = []
+
     if request.method == 'POST':
-        anotacao = request.form['conteudo']
-        if anotacao is None or anotacao == ' ':
+        note = request.form['content']
+
+        if note is None or note == ' ':
             return 404
-        session["notas"].append(anotacao)
-        return render_template('PaginaInicial.html', note=session["notas"],notinha=anotacao)
+        storage.append(note)
+        return render_template('PaginaInicial.html', storage=storage)
+
     else:
-        return render_template('PaginaInicial.html', note=session["notas"])
+        return render_template('PaginaInicial.html', storage=storage)
+
 
 @app.route('/back/<delete>', methods=['POST','GET'])
 def back(delete):
+
     if request.method == 'POST':
-        session["notas"].remove(delete)
+        storage.remove(delete)
         return redirect(url_for('notes'))
+
     else:
         return render_template("stop.html")
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
